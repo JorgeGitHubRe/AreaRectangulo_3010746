1299e7d1446b519ca3ac92170c254d9ca4454ba0
Jorge <Jorge@DESKTOP-5DL7PLT>
2023-08-19 18:41:18 -0600

    Agregar archivos de proyecto.

---
+++ AreaRectangulo_3010746/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace AreaRectangulo_3010746
+{
+    public partial class MainPage : ContentPage
+    {
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double b = Convert.ToDouble(bas.Text);
+            double a = Convert.ToDouble(altu.Text);
+
+            p.Text = "El perímetro es: " + (b + b + a + a);
+            s.Text = "La superficie es: " + (b * a);
+        }
+    }
+}
